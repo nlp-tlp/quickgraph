daf3b87daabaf3b28b6e6c7664ccc950661ce87b
4theKnowledge <tylerbikaun@gmail.com>
2024-11-04 20:28:22 +0800

    fix: update API base URL and Dockerfile configuration for nginx

--- client/src/shared/utils/api.js
+++ client/src/shared/utils/api.js
@@ -2,7 +2,8 @@
 
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
 // Custom error for missing token
 class TokenError extends Error {
