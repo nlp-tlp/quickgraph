1d3828d61b8d69a47c58121a78b5d82a7fa91d84
4theKnowledge <tylerbikaun@gmail.com>
2024-11-04 16:29:14 +0800

    fix: add additional origins for CORS support

--- server/src/quickgraph/main.py
+++ server/src/quickgraph/main.py
@@ -43,6 +43,10 @@ tags_metadata = [
 
 origins = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://client",
 ]
 
 
